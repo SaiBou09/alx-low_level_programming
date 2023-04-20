f64836b6647193fd76fa78f175b1b9b6c36f7333
SaiBou09 <saidabouazza09@gmail.com>
2023-04-20 13:46:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programm that prints a name with puts function
+ * Return: O (Success)
+ */
+int main (void)
+{
+	puts ("\"Programming is like building a multilingual puzzle");
+        Return(0);
+}
