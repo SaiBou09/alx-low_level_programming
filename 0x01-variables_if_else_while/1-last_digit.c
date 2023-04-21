e94482be332183f02e18a5b4ed2772744212c4d2
SaiBou09 <saidabouazza09@gmail.com>
2023-04-21 12:33:50 -0700

    h

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ last_digit = abs( n % 10);
 printf("Last digit of %d is ", n);
 if (last_digit > 5)
 {
-printf("%d and is greater than 5\n", last_digit);
+printf("Last digit of %d is and is greater than 5\n", last_digit);
 }
 else if (last_digit == 0)
 {
-printf("%d and is 0", last_digit);
+printf("Last digit of %d is and is 0", last_digit);
 }
 else
 {
-printf("%d is less than 6 and not 0", last_digit);
+printf("Last digit of %d is and is less than 6 and not 0", last_digit);
 }
 return (0);
 }
