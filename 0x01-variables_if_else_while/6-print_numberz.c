2276c1a6491c2ffc38e58e06d776775eacc0b8b7
SaiBou09 <saidabouazza09@gmail.com>
2023-04-22 05:03:22 -0700

    6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,13 +4,14 @@
  * Description: A program that prints single digits numbers
  * Of base 10, starting from O
  * followed by a new line
+ * Return: always 0
  */
 int main(void)
 {
 int i;
-for (i = 0; i <= 9; i++)
+for (i = '0'; i <= '9'; i++)
 {
-putchar(i + 0);
+putchar(i);
 }
 putchar('\n');
 return (0);
