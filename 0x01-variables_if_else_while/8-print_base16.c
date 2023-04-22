064ebc32c541043071f3aa4d6705102a023413ee
SaiBou09 <saidabouazza09@gmail.com>
2023-04-22 05:39:31 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ * Description: A program that print all numbers of base 16
+ * Followed by a new line
+ * Return: always 0
+ */
+int main(void)
+{
+char c;
+for (c = 0; c <= 9; c++)
+{
+putchar(c);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
