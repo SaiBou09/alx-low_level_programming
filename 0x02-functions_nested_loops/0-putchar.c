ac3129533a574a7cd27ace90b024348624d3738f
SaiBou09 <saidabouazza09@gmail.com>
2023-04-25 12:01:19 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main -Entry point
+ * Description: A program that prints _putchar
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for (ch = ; ch < 8; c++)
+{
+_putchar(str[ch]);
+_putchar("\n");
+return (0);
+}
