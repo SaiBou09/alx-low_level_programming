263c1bf8d158419876e088ed18c39a66d714789d
SaiBou09 <saidabouazza09@gmail.com>
2023-04-25 13:26:10 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet  - utilizes _putchar function
+ *                   to  print the alphabet, in lowercase
+ *                   from a to z, followed by a new line
+ */
+
+void print_alphabet(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+_putchar('\n');
+}
