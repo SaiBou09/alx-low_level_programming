522864f432c7638b6810cf2b0a23f7e4a0a41714
SaiBou09 <saidabouazza09@gmail.com>
2023-04-26 07:45:20 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - Entry type
+ * Description: prints alphabet 10*, folllowed by \n
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+int ln;
+int cn;
+for (ln = 0; ln <= 9; ln++)
+{
+_putchar('\n');
+for (cn = 'a'; cn <= 'z'; cn++)
+_putchar(cn);
+_putchar('\n');
+}
+}
