a2f271c193592f43b4a032761ce87416c2ecfac5
SaiBou09 <saidabouazza09@gmail.com>
2023-04-27 14:24:45 -0700

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: takes integer as input
+ */
+
+void print_line(int n)
+{
+int line;
+if (n <= 0)
+_putchar('\n');
+else
+{
+for ( line = 0; line <= n; line++)
+_putchar('_');
+_putchar('\n');
+}
+}
