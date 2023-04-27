438f51da29b2e36e764c1273f0227d0eedef1855
SaiBou09 <saidabouazza09@gmail.com>
2023-04-27 03:55:26 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  _isupper - checks for uppercase characters
+ *  @c: takes integer as input for function
+ *
+ *  Return: 1 if uppercase
+ *          0 if elsewise
+ */
+
+int _isupper(int c)
+{
+for (c = 'A'; c <= 'Z'; c++)
+return (1);
+return (0);
+}
