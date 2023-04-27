dbc6e8453780b6ba895ec6dd5d82c4cf380f80ba
SaiBou09 <saidabouazza09@gmail.com>
2023-04-27 13:49:28 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,7 @@
  * print_most_numbers - prints the numbers 0 to 9, except 2 and 4
  *                      followed by a new line
  *
- *Return: Always (0)
+ * Return: Always (0)
  */
 
 void print_most_numbers(void)
