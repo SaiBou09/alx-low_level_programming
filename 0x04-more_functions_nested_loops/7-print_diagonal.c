f575d878c073db5bb89334623fb4f49a3961267b
SaiBou09 <saidabouazza09@gmail.com>
2023-04-27 14:45:27 -0700

    7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,17 @@
 
 void print_diagonal(int n)
 {
-int lnChr;
+int lnChr, space;
 if (n <= 0)
 _putchar('\n');
 else
 {
 for (lnChr = 1; lnChr <= n, lnChr++)
-_putchar('\');
+{
+for (space = 1, space <= lnChr, space++)
+_putchar(' ');
+_putchar(92); /*is equal to \ char*/
 _putchar('\n');
+}
 }
 }
