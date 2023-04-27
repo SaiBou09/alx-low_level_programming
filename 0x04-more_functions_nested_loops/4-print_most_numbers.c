6f0673ecfd3a843746cccfd50f31f865c72a3aa5
SaiBou09 <saidabouazza09@gmail.com>
2023-04-27 13:53:23 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,12 +9,12 @@
 
 void print_most_numbers(void)
 {
-int c;
-for (c = '0'; c <= '9'; c++)
+int num;
+for (c = 0; c <= 9; c++)
 {
-if (c == '2' || c == '4')
+if (c == 2 || c == 4)
 continue;
-_putchar(c + '0');
+_putchar(c + 48);
 }
 _putchar('\n');
 }
