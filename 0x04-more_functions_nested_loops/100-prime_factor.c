820bf6717e3855250c7c322508e3e00cdf15b8e8
SaiBou09 <saidabouazza09@gmail.com>
2023-04-28 15:13:58 -0700

    100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -21,3 +21,39 @@ sqrt = (x / tmp + tmp) / 2;
 }
 return (sqrt);
 }
+
+/**
+ * largest_prime_factor - finds and prints the largest
+ *                        prime factor of number (num)
+ *
+ * @num : number to find
+*/
+
+void largest_prime_factor(long int num)
+{
+int prmNu, largest;
+
+/* first divide with the smallest prime number  (two) */
+while (num % 2 == 0)
+{
+num = num / prmNu;
+largest = prmNu;
+}
+}
+if (num > 2)
+largest = num;
+printf("%d\n", largest);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: always (0)
+*/
+
+int main(void)
+{
+largest_prime_factor(612852475143);
+
+return (0);
+}
