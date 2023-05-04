2f2456079f518618a109cbd2afe83058ffd2dc21
SaiBou09 <saidabouazza09@gmail.com>
2023-05-04 04:42:30 -0700

    3

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,15 +8,11 @@
 
 char *_strcat(char *dest, char *src)
 {
-char *p = dest;
-while (*p)
-{
-*p++;
-}
-while (*src)
-{
-*p++ = *src++;
-}
-*p = '\0';
+int a, b;
+a = 0;
+while (dest[a])
+a++;
+for (b = 0; src[b]; b++)
+dest[a++] = src[b];
 return (dest);
 }
