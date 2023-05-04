1365a6e7b6963fc7d0ed460f68c93e130a3e59fd
SaiBou09 <saidabouazza09@gmail.com>
2023-05-04 04:53:49 -0700

    1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings
+ * @dest: destination
+ * @src: source
+ * @n: parameter
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+int dest_len = strlen(dest);
+int i;
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
+dest[dest_len + i] = src[i];
+dest[dest_len + i] = '\0';
+return (dest);
+}
