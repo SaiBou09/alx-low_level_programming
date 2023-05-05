6a21b235f2762b105f70663b4bd8c79079e227b8
SaiBou09 <saidabouazza09@gmail.com>
2023-05-05 02:33:47 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copies a string
+ * @dest: string to be copied in
+ * @src: string to copy from
+ * @n: lenth of src
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
+}
