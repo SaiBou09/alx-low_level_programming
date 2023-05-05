574db2c1f3908a97e7b767e8a587211e9de0c468
SaiBou09 <saidabouazza09@gmail.com>
2023-05-05 02:52:40 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,12 +4,12 @@
  * _strcmp - compares 2 strings
  * @s1: first string to compare
  * @s2: second string to compare
- * Return: less than, equal orgreater than 0
+ * Return: 1 if true, 0 if false
  */
 
 int _strcmp(char *s1, char *s2)
 {
-int i;
+int i = 0;
 while (*s1)
 {
 if (*s1 != *s2)
